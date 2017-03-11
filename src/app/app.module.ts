5c832d2529827470e0422a5b26f564463e6ae99f
Erin Kim <kim.erin.kim@gmail.com>
2017-03-11 15:03:26 -0500

    bootstrap material design working correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list.component';
 
 import { SharedService } from './shared/shared.service';
 import { FilterPipe } from './filter.pipe';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeDetailComponent } from './employee-detail.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { EmployeeDetailComponent } from './employee-detail/employee-detail.compo
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+
   ],
   providers: [ SharedService, FilterPipe ],
   bootstrap: [AppComponent],
